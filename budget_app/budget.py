3e02f3ee5551b08bff8c4b890e550935e25a45e0
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-05-10 18:05:50 -0300

    implemented create_spend_chart function

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -29,7 +29,7 @@ class Category:
     def check_funds(self, amount):
         return self.get_balance() >= amount
     
-    def __str__(self):   #TO DO
+    def __str__(self):   
 
         result = f'{self.name.center(30, "*")}\n'
         total = 0
@@ -45,16 +45,109 @@ class Category:
 
 
 def create_spend_chart(categories):
-    return True
+    categories_stats = list()
 
-'''
-food = Category("Food")
-bar = Category("dog")
+    chart_width = len(categories) + 7
 
-food.deposit(200, "deposit")
-bar.deposit(4500, "more than twenty three words I think")
-food.transfer(100, bar)
+    def calculate_total_spend(categories):
+        total = 0
+        for cateogory in categories:
+            for ledge in cateogory.ledger:
+                if ledge["amount"] < 0:
+                    total = total + (ledge["amount"] * -1)
+        return total
 
-print(food)
-print(bar)
-'''
+
+    total = calculate_total_spend(categories)
+    
+
+    def generate_category_bar(category, percentage, first_category = False):
+        category_bar = list()
+
+        for char in range(100, -10, -10):
+            if char > percentage:
+                if first_category:
+                    category_bar.append(" ")
+                else:
+                    category_bar.append("  ")
+            else:
+                if first_category:
+                    category_bar.append("o")
+                else:
+                    category_bar.append(" o")
+
+        return category_bar
+
+    def poblate_categories_stats(categories, total):
+        for index, category in enumerate(categories):
+            category_total = 0
+            name = category.name
+
+            for ledge in category.ledger:
+                if ledge["amount"] < 0:
+                    category_total = category_total + (ledge["amount"] * -1)
+            
+            percentage_spent = int((category_total / total) * 100)
+
+            if index == 0:
+                category_bar = generate_category_bar(category, percentage_spent, True)
+            else:
+                category_bar = generate_category_bar(category, percentage_spent)
+
+            category_stats = {"name": name, "category_bar": category_bar}
+
+            categories_stats.append(category_stats)
+
+    
+    #Hard code legend margin
+    legend = []
+    for char in range(100, -10, -10):
+        legend.append(f"{char}|")
+    
+    #Hard code horizonal line
+    horizonal_line = ""
+    for char in range(chart_width):
+        horizonal_line = horizonal_line + "-"
+
+    poblate_categories_stats(categories, total)
+
+    chart = ""
+    for index, number in enumerate(legend):
+        categories_bar = ""
+        for category in categories_stats:
+            categories_bar = categories_bar + " " + category["category_bar"][index]
+        if number == "100|":
+            chart = chart + f"{number}{categories_bar}\n"
+        elif number == "0|":
+            chart = chart + f"  {number}{categories_bar}\n"
+        else:
+            chart = chart + f" {number}{categories_bar}\n"
+
+    chart = chart + f"    {horizonal_line}"
+
+    ##longest word?
+    longest_category_name_lenght = len(categories[0].name)
+    for category in categories:
+        if len(category.name) > longest_category_name_lenght:
+            longest_category_name_lenght = len(category.name)
+
+    bottom_chart = ""
+    for index in range(longest_category_name_lenght):
+        name_bar = ""
+        for category_number, category in enumerate(categories):
+            name_len = len(category.name)
+            if name_len > index:
+                if category_number == 0:
+                    name_bar = name_bar + f" {category.name[index]}"
+                else:
+                    name_bar = name_bar + f"  {category.name[index]}"
+            else:
+                if category_number == 0:
+                    name_bar = name_bar + "  "
+                else:
+                    name_bar = name_bar + "   "
+        bottom_chart = bottom_chart + f"    {name_bar}\n"
+
+    bottom_chart = bottom_chart[:-1]
+    chart = "Percentage spent by category\n" + chart + f"\n{bottom_chart}"
+    return type(chart)
