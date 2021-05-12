04c5a65cad50b05c99ace277b6a896f739acad7f
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-05-12 17:22:31 -0300

    finished probability_calculator

--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -16,7 +16,7 @@ class Hat:
     def draw(self, amount):
         top = amount
         balls_drawed = list()
-        if top > len(self.contents):
+        if top >= len(self.contents):
             balls_drawed = self.contents
         else:
             while(top > 0):
@@ -28,9 +28,23 @@ class Hat:
     
 
 
-#def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    m = 0
+    the_expected_balls = list()
+    for color in expected_balls:
+        for number in range(expected_balls[color]):
+            the_expected_balls.append(color)
+    #the_expected_balls.sort()
 
-hat1 = Hat(yellow=3, blue=2, green=6)
-print(hat1.hat)
-print(hat1.contents)
-print(hat1.draw(50))
+
+
+    for experiment in range(num_experiments):
+        copy_hat = copy.copy(hat)
+        returned_balls = copy_hat.draw(num_balls_drawn)
+        #returned_balls.sort()
+        #if the_expected_balls in returned_balls:
+        if all(elem in the_expected_balls for elem in returned_balls):
+            m = m +1
+    
+    return m / num_experiments
+
